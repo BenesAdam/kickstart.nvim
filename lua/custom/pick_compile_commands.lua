fdced00feef94a02dda21e7bd0be3fa546ee3507
Adam Benes <adam.benes.external@zf.com>
2025-05-13 19:11:39 +0200

    global compile command dir and path ready to use

--- lua/custom/pick_compile_commands.lua
+++ lua/custom/pick_compile_commands.lua
@@ -1,4 +1,5 @@
-local clangdCommand = nil
+local compileCommandsDir = nil
+local compileCommandsPath = nil
 
 function PickCompileCommands()
   local telescope = require 'telescope.builtin'
@@ -15,11 +16,8 @@ function PickCompileCommands()
         actions.close(prompt_bufnr)
         local entry = action_state.get_selected_entry()
         local dir = vim.fn.fnamemodify(entry.path, ':h')
-        dir = vim.fs.normalize(dir)
-        local normalized_path = dir .. '/compile_commands.json'
-
-        -- Set clangd command
-        clangdCommand = { 'clangd', '--compile-commands-dir=' .. dir }
+        compileCommandsDir = vim.fs.normalize(dir)
+        compileCommandsPath = compileCommandsDir .. '/compile_commands.json'
 
         -- Restart clangd clients
         for _, client in ipairs(vim.lsp.get_clients()) do
@@ -34,12 +32,12 @@ function PickCompileCommands()
         if vim.api.nvim_buf_get_name(0) ~= '' then
           vim.cmd 'edit'
         else
-          vim.cmd('e ' .. normalized_path)
+          vim.cmd('e ' .. compileCommandsPath)
         end
 
         -- Print out new compile commands
         vim.defer_fn(function()
-          vim.notify(normalized_path, vim.log.levels.INFO)
+          vim.notify(compileCommandsPath, vim.log.levels.INFO)
         end, 200)
       end)
 
@@ -51,8 +49,8 @@ end
 -- Setting of new clangd clients
 require('lspconfig').clangd.setup {
   on_new_config = function(new_config, root_dir)
-    if clangdCommand then
-      new_config.cmd = clangdCommand
+    if compileCommandsDir then
+      new_config.cmd = { 'clangd', '--compile-commands-dir=' .. compileCommandsDir}
     end
   end,
 }
