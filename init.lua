2dcd9929b3a26733359e2c64bf11db3b17b62065
Adam Benes <adam.benes.external@zf.com>
2025-05-26 12:04:00 +0200

    clang-format integrated and working

--- init.lua
+++ init.lua
@@ -706,6 +706,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'clang-format', -- Used to format c/c++ code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -758,11 +759,23 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        c = { 'clang_format' },
+        cpp = { 'clang_format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+
+      formatters = {
+        clang_format = {
+          command = 'clang-format',
+          -- prepend_args = { '--style={BasedOnStyle: Microsoft, IndentWidth: 2, ColumnLimit: 0, SortIncludes: false, ReflowComments: false}' },
+          prepend_args = {
+            '--style={BasedOnStyle: llvm, IndentWidth: 2, ColumnLimit: 0, SortIncludes: false, ReflowComments: false, BreakBeforeBraces: Allman, AllowShortIfStatementsOnASingleLine: false, IndentCaseLabels: false, TabWidth: 2, NamespaceIndentation: All, PointerAlignment: Left, FixNamespaceComments: false, BreakConstructorInitializers: AfterColon, ConstructorInitializerIndentWidth: 2}',
+          },
+        },
       },
     },
   },
