a1d7e1cca4e93ff47e51c3e69266779c894c9127
Adam Beneš <adam.bilovice@seznam.cz>
2026-05-10 20:33:50 +0200

    Command VimPackRestore added.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,6 +6,14 @@
 -- Add :LspInfo alias
 vim.api.nvim_create_user_command('LspInfo', 'checkhealth vim.lsp', {})
 
+-- Restore based on nvim-pack-lock.json
+vim.api.nvim_create_user_command("VimPackRestore", function()
+  vim.pack.update(nil, {
+    target = "lockfile",
+    force = true,
+  })
+end, {})
+
 -- Iterate over all Lua files in the plugins directory and load them
 local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'custom', 'plugins')
 for file_name, type in vim.fs.dir(plugins_dir) do
